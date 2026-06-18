f37419b833a0584f2538881b6c4f457f339b52a7
Benjamin-66Tar <bemlopez200613@gmail.com>
2026-06-17 21:25:16 -0600

    comentario

--- InventoryPro/settings.py
+++ InventoryPro/settings.py
@@ -52,7 +52,7 @@ ROOT_URLCONF = "InventoryPro.urls" # Asegúrate que este nombre coincida con tu 
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3", # nombre de la base de datos
     }
 }
 
