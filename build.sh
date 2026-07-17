6863fd53eaf6f7761ad4259c61117194bfc8a48b
Benjamin-66Tar <bemlopez200613@gmail.com>
2026-07-16 22:40:38 -0600

    compilando en el frontend

---
+++ build.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+# Exit on error
+set -o errexit
+
+echo "=== Instalando dependencias de Python ==="
+pip install -r requirements.txt
+
+echo "=== Instalando y compilando React Frontend ==="
+npm install --prefix frontend
+npm run build --prefix frontend
+
+echo "=== Recopilando archivos estáticos de Django ==="
+python manage.py collectstatic --no-input
+
+echo "=== Aplicando migraciones de la base de datos ==="
+python manage.py migrate
